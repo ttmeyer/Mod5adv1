0554868b84ba0e9dc0383b082efb07dd29460cb4
ttmeyer <33078295+ttmeyer@users.noreply.github.com>
2017-12-13 20:37:11 -0600

    Complete Comb through Index and user

--- user.php
+++ user.php
@@ -1,16 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Tenant Communicator</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" rel="stylesheet" type="text/css"/>
-<link href="styles/custom.css" rel="stylesheet" type="text/css">
-<script src="http://code.jquery.com/jquery-1.8.3.min.js" type="text/javascript"></script>
-<script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js" type="text/javascript"></script>
-</head>
 
 <body>
 
@@ -32,21 +20,17 @@ $result = mysqli_query($conn, $query);
 if (mysqli_num_rows($result) > 0) {
 	while ($row = mysqli_fetch_assoc($result)) {
 
-		//$file = fopen ("Landlord.html","r")
-		//header('location: index.html');
-
 		header("location: http://tenantcommunicator.azurewebsites.net/landlord.html");
-		//echo "true";
-
 		}
 
 		} else {
-		//$file = fopen ("index.html","r")
-		//header("location: Landlord.html");
-		header("location: http://tenantcommunicator.azurewebsites.net/index.html");
+
+ 		header("location: http://tenantcommunicator.azurewebsites.net/index.html");
 		}
 
 mysqli_close($conn);
+
+?>
 
  </body>
  </html>
