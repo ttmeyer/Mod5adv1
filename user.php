3654dba7d6bdc8a10baf6089a051a5c5ffde416d
ttmeyer <33078295+ttmeyer@users.noreply.github.com>
2017-12-16 20:23:09 -0600

    Bringing it all back

---
+++ user.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+-<html lang="en">
+
+<body>
+
+<?php
+
+ // connect to server
+	include 'open_config.php';
+
+//	session_start();
+	$username = (isset($_POST['UserName'])    ? $_POST['UserName']   : '');
+	$password = (isset($_POST['Password'])    ? $_POST['Password']   : '');
+
+$query = "SELECT UserName
+FROM user
+WHERE UserName = '$username' AND Password='$password' LIMIT 1";
+
+$result = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($result) > 0) {
+	while ($row = mysqli_fetch_assoc($result)) {
+
+		header("location: http://tenantcommunicator.azurewebsites.net/landlord1.html");
+
+		}
+
+		} else {
+
+ 		header("location: http://tenantcommunicator.azurewebsites.net/index.html");
+		}
+
+mysqli_close($conn);
+
+?>
+
+ </body>
+ </html>
