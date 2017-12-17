b4e1c86449394903f5b094a284c2e3f8330fc0aa
ttmeyer <33078295+ttmeyer@users.noreply.github.com>
2017-12-16 18:56:00 -0600

    desperation

--- user.php
+++ user.php
@@ -1,16 +1,5 @@
 <!DOCTYPE html>
 -<html lang="en">
--<head>
--<meta charset="utf-8">
--<meta http-equiv="X-UA-Compatible" content="IE=edge">
--<meta name="viewport" content="width=device-width, initial-scale=1">
--<title>Tenant Communicator</title>
--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
--<link href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" rel="stylesheet" type="text/css"/>
--<link href="styles/custom.css" rel="stylesheet" type="text/css">
--<script src="http://code.jquery.com/jquery-1.8.3.min.js" type="text/javascript"></script>
--<script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js" type="text/javascript"></script>
--</head>
 
 <body>
 
@@ -33,7 +22,7 @@ if (mysqli_num_rows($result) > 0) {
 	while ($row = mysqli_fetch_assoc($result)) {
 
 		header("location: http://tenantcommunicator.azurewebsites.net/landlord1.html");
-		
+
 		}
 
 		} else {
