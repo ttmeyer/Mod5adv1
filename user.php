5d8d2e76a54a928d85319929e7a534d4805d69d2
ttmeyer <33078295+ttmeyer@users.noreply.github.com>
2017-12-17 17:17:47 -0600

    trying without url

--- user.php
+++ user.php
@@ -16,7 +16,8 @@ $result = mysqli_query($conn, $query);
 
  if (mysqli_num_rows($result) > 0) {
 	while ($row = mysqli_fetch_assoc($result)) {
-header('location: http://tenantcommunicator.azurewebsites.net/landlord.html');
+header('location: /landlord.html');
+// http://tenantcommunicator.azurewebsites.net/landlord.html');
 		}
 
 		} else {
